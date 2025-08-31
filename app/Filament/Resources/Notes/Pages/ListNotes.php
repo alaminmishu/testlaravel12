bdf35dc75bf5532ac8ca342798aa5d23380b1d27
alaminmishu <aaamishucse@gmail.com>
2025-08-31 19:03:59 +0600

    sample notes app

---
+++ app/Filament/Resources/Notes/Pages/ListNotes.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Notes\Pages;
+
+use App\Filament\Resources\Notes\NoteResource;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Schemas\Components\Tabs\Tab;
+
+class ListNotes extends ListRecords
+{
+    protected static string $resource = NoteResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'active' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('active', true)),
+            'inactive' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('active', false)),
+        ];
+    }
+}
---
+++ app/Filament/Resources/Notes/Schemas/NoteForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Notes\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class NoteForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                MarkdownEditor::make('content')
+                    ->default(null)
+                    ->columnSpanFull(),
+                Toggle::make('active')
+                    ->required(),
+            ]);
+    }
+}
